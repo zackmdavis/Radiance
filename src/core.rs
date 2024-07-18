96198e1121c9555a839c88ba057ab9706edc8a19
Zack M. Davis <code@zackmdavis.net>
2024-07-18 16:02:37 -0700

    backpropagation

--- src/core.rs
+++ src/core.rs
@@ -1,4 +1,5 @@
 use std::cell::RefCell;
+use std::collections::HashMap;
 use std::fmt;
 use std::hash::{Hash, Hasher};
 use std::rc::Rc;
@@ -24,7 +25,6 @@ struct Tensor {
     array: ArrayD<f32>,
     requires_gradient: bool,
     gradient: RefCell<Option<ArrayD<f32>>>,
-    #[allow(dead_code)]
     origin: Option<Origin>,
 }
 
@@ -33,7 +33,6 @@ impl fmt::Debug for Tensor {
         f.debug_struct("Tensor")
             .field("identifier", &self.identifier)
             .field("array", &self.array)
-            .field("requires_gradient", &self.requires_gradient)
             .field("gradient", &self.gradient)
             .finish()
     }
@@ -57,18 +56,15 @@ struct TensorBuilder {
     array: ArrayD<f32>,
     identifier: Option<String>,
     requires_gradient: bool,
-    gradient: Option<ArrayD<f32>>,
     origin: Option<Origin>,
 }
 
-#[allow(dead_code)]
 impl TensorBuilder {
     fn new(array: ArrayD<f32>) -> TensorBuilder {
         TensorBuilder {
             array,
             identifier: None,
             requires_gradient: true,
-            gradient: None,
             origin: None,
         }
     }
@@ -80,11 +76,6 @@ impl TensorBuilder {
 
     fn requires_gradient(mut self, requires: bool) -> TensorBuilder {
         self.requires_gradient = requires;
-        self
-    }
-
-    fn gradient(mut self, gradient: ArrayD<f32>) -> TensorBuilder {
-        self.gradient = Some(gradient);
         self
     }
 
@@ -101,16 +92,14 @@ impl TensorBuilder {
                 None => generate_sequential_tensor_id(),
             },
             requires_gradient: self.requires_gradient,
-            gradient: RefCell::new(self.gradient),
+            gradient: RefCell::new(None),
             origin: self.origin,
         }
     }
 }
 
 struct Origin {
-    #[allow(dead_code)]
     operation: Box<dyn Operation>,
-    #[allow(dead_code)]
     parents: Vec<Rc<Tensor>>,
 }
 
@@ -118,7 +107,7 @@ trait Operation {
     fn forward(&self, inputs: Vec<Rc<Tensor>>) -> Rc<Tensor>;
     fn backward(
         &self,
-        _out_gradient: ArrayD<f32>,
+        _out_gradient: &ArrayD<f32>,
         _args: Vec<Rc<Tensor>>,
         _arg_index: usize,
     ) -> ArrayD<f32>;
@@ -138,12 +127,12 @@ impl Operation for Addition {
     }
     fn backward(
         &self,
-        out_gradient: ArrayD<f32>,
+        out_gradient: &ArrayD<f32>,
         _args: Vec<Rc<Tensor>>,
         _arg_index: usize,
     ) -> ArrayD<f32> {
         // Addition just passes the gradient through to both branches.
-        out_gradient
+        out_gradient.clone()
     }
 }
 
@@ -160,20 +149,19 @@ impl Operation for Multiplication {
     }
     fn backward(
         &self,
-        out_gradient: ArrayD<f32>,
+        out_gradient: &ArrayD<f32>,
         args: Vec<Rc<Tensor>>,
         arg_index: usize,
     ) -> ArrayD<f32> {
         let other_arg_index = match arg_index {
             0 => 1,
             1 => 0,
-            _ => panic!("binary operation expected")
+            _ => panic!("binary operation expected"),
         };
         // d/dx(xy) = y
         out_gradient * args[other_arg_index].array.clone() // dubious perf &c.
     }
 }
-
 
 fn register_parents(sorter: &mut TopologicalSort<Rc<Tensor>>, child: Rc<Tensor>) {
     if let Some(origin) = &child.origin {
@@ -184,7 +172,6 @@ fn register_parents(sorter: &mut TopologicalSort<Rc<Tensor>>, child: Rc<Tensor>)
     }
 }
 
-#[allow(dead_code)]
 fn sorted_computation_graph(end: Rc<Tensor>) -> Vec<Rc<Tensor>> {
     let mut sorter = TopologicalSort::new();
     register_parents(&mut sorter, end);
@@ -194,11 +181,40 @@ fn sorted_computation_graph(end: Rc<Tensor>) -> Vec<Rc<Tensor>> {
     sorted
 }
 
-#[allow(dead_code)]
-fn backprop(_culmination: Rc<Tensor>) {
-    // TODO
-}
+fn backprop(culmination: Rc<Tensor>) {
+    let mut gradients = HashMap::<String, ArrayD<f32>>::new();
+    gradients.insert(culmination.identifier.clone(), array![1.].into_dyn());
+    for node in sorted_computation_graph(culmination) {
+        let gradient = gradients
+            .remove(&node.identifier)
+            .expect("gradient should be stored");
 
+        // If it requires gradient, set it from the map
+        if node.requires_gradient {
+            *node.gradient.borrow_mut() = Some(gradient.clone());
+        }
+
+        // If it has an origin, use the backward function to accumulate
+        // gradients for the parents in our map
+        if let Some(origin) = &node.origin {
+            let out_gradient = gradient;
+            for (i, parent) in origin.parents.iter().enumerate() {
+                let contribution =
+                    origin
+                        .operation
+                        .backward(&out_gradient, origin.parents.clone(), i);
+                match gradients.get_mut(&parent.identifier) {
+                    Some(gradient) => {
+                        *gradient = &*gradient + contribution;
+                    }
+                    None => {
+                        gradients.insert(parent.identifier.clone(), contribution);
+                    }
+                }
+            }
+        }
+    }
+}
 
 #[test]
 fn test_addition_forward() {
@@ -222,8 +238,14 @@ fn test_addition_backward() {
     let b = TensorBuilder::new(array![2.].into_dyn()).build();
     let args = vec![Rc::new(a), Rc::new(b)];
     let out_gradient = array![1.].into_dyn();
-    assert_eq!(Addition{}.backward(out_gradient.clone(), args.clone(), 0), out_gradient);
-    assert_eq!(Addition{}.backward(out_gradient.clone(), args.clone(), 1), out_gradient);
+    assert_eq!(
+        Addition {}.backward(&out_gradient, args.clone(), 0),
+        out_gradient
+    );
+    assert_eq!(
+        Addition {}.backward(&out_gradient, args.clone(), 1),
+        out_gradient
+    );
 }
 
 #[test]
@@ -232,11 +254,95 @@ fn test_multiplication_backward() {
     let b = TensorBuilder::new(array![3.].into_dyn()).build();
     let args = vec![Rc::new(a), Rc::new(b)];
     let out_gradient = array![1.].into_dyn();
-    assert_eq!(Multiplication{}.backward(out_gradient.clone(), args.clone(), 0), array![3.].into_dyn());
-    assert_eq!(Multiplication{}.backward(out_gradient.clone(), args.clone(), 1), array![2.].into_dyn());
+    assert_eq!(
+        Multiplication {}.backward(&out_gradient, args.clone(), 0),
+        array![3.].into_dyn()
+    );
+    assert_eq!(
+        Multiplication {}.backward(&out_gradient, args.clone(), 1),
+        array![2.].into_dyn()
+    );
 }
 
 #[test]
 fn test_backprop() {
-    // TODO
+    // Test written by Claude 3.5 Sonnet
+    let a = Rc::new(
+        TensorBuilder::new(array![2.0].into_dyn())
+            .identifier("a".to_string())
+            .requires_gradient(true)
+            .build(),
+    );
+    let b = Rc::new(
+        TensorBuilder::new(array![3.0].into_dyn())
+            .identifier("b".to_string())
+            .requires_gradient(true)
+            .build(),
+    );
+    let c = Rc::new(
+        TensorBuilder::new(array![4.0].into_dyn())
+            .identifier("c".to_string())
+            .requires_gradient(true)
+            .build(),
+    );
+
+    let mul = Multiplication {}.forward(vec![a.clone(), b.clone()]);
+    let result = Addition {}.forward(vec![mul, c.clone()]);
+
+    backprop(result);
+
+    assert_eq!(
+        *a.gradient.borrow().as_ref().unwrap(),
+        array![3.0].into_dyn()
+    );
+    assert_eq!(
+        *b.gradient.borrow().as_ref().unwrap(),
+        array![2.0].into_dyn()
+    );
+    assert_eq!(
+        *c.gradient.borrow().as_ref().unwrap(),
+        array![1.0].into_dyn()
+    );
+}
+
+#[test]
+fn test_backprop_with_reuse() {
+    // Test written by Claude 3.5 Sonnet
+    let a = Rc::new(
+        TensorBuilder::new(array![2.0].into_dyn())
+            .identifier("a".to_string())
+            .requires_gradient(true)
+            .build(),
+    );
+    let b = Rc::new(
+        TensorBuilder::new(array![3.0].into_dyn())
+            .identifier("b".to_string())
+            .requires_gradient(true)
+            .build(),
+    );
+
+    // Compute (a * b) + (a + b)
+    let mul = Multiplication {}.forward(vec![a.clone(), b.clone()]);
+    let add = Addition {}.forward(vec![a.clone(), b.clone()]);
+    let result = Addition {}.forward(vec![mul, add]);
+
+    backprop(result);
+
+    // Gradient for 'a':
+    // From (a * b): derivative is b = 3
+    // From (a + b): derivative is 1
+    // Total: 3 + 1 = 4
+    assert_eq!(
+        *a.gradient.borrow().as_ref().unwrap(),
+        array![4.0].into_dyn()
+    );
+
+    // Gradient for 'b':
+    // From (a * b): derivative is a = 2
+    // From (a + b): derivative is 1
+    // Total: 2 + 1 = 3
+    assert_eq!(
+        *b.gradient.borrow().as_ref().unwrap(),
+        array![3.0].into_dyn()
+    );
 }
