d79115b60241851cd7d991e8b8860e779f33a82d
Zack M. Davis <code@zackmdavis.net>
2024-08-03 16:07:00 -0700

    cursory attention head testing

--- src/core/attention.rs
+++ src/core/attention.rs
@@ -99,6 +99,7 @@ impl AttentionHead {
     pub fn forward(&self, x: Rc<Tensor>) -> Rc<Tensor> {
         // Input is shape (sequence_length, embedding_dimensionality).
         let n = x.array.borrow().shape()[0];
+        assert!(n >= 1, "need at least one token");
 
         // Queries, keys, and values are each shape (embedding_dimensionality, attention_dimensionality).
         // So XW_q (respectively _k, _v) are shape (sequence_length, attention_dimensionality)
@@ -112,7 +113,15 @@ impl AttentionHead {
         let scale =
             Rc::new(TensorBuilder::new((scale_factor * Array::ones((n, n))).into_dyn()).build());
         let scaled_qk_t = Multiplication {}.forward(vec![scale, qk_t]);
-        let mask = Rc::new(TensorBuilder::new(Array::ones((n, n)).tril(0).into_dyn()).build());
+
+        // `tril` chokes on 1×1: https://github.com/rust-ndarray/ndarray/issues/1415
+        let raw_mask = if n == 1 {
+            Array::ones((n, n))
+        } else {
+            Array::ones((n, n)).tril(0)
+        };
+
+        let mask = Rc::new(TensorBuilder::new(raw_mask.into_dyn()).build());
         let masked = Mask {}.forward(vec![scaled_qk_t, mask]);
         let softmaxed = SoftmaxRows {}.forward(vec![masked]);
         let h = MatrixMultiplication {}.forward(vec![softmaxed, v]);
@@ -124,4 +133,93 @@ pub struct AttentionMultiHead {
     heads: Vec<AttentionHead>,
     output_weights: Rc<Tensor>,
     // TODO: MLP
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dimensionalities() {
+        let head = AttentionHead::new("my_first_attention_head", 64, 16);
+        assert_eq!(head.embedding_dimensionality(), 64);
+        assert_eq!(head.attention_dimensionality(), 16);
+    }
+
+    #[test]
+    fn test_input() {
+        let head = AttentionHead::new("test_attention_head", 8, 4);
+        let input = Rc::new(
+            TensorBuilder::new(
+                array![
+                    [1., 2., 3., 4., 5., 6., 7., 8.],
+                    [9., 8., 7., 6., 5., 4., 3., 2.]
+                ]
+                .into_dyn(),
+            )
+            .build(),
+        );
+        head.forward(input);
+    }
+
+    #[test]
+    fn test_single_token_input() {
+        let head = AttentionHead::new("single_token_test_attention_head", 8, 4);
+        let input = Rc::new(
+            TensorBuilder::new(array![[1., 2., 3., 4., 5., 6., 7., 8.]].into_dyn()).build(),
+        );
+        head.forward(input);
+    }
+
+    #[test]
+    fn test_tiny_input() {
+        let head = AttentionHead::new("single_token_test_attention_head", 8, 4);
+        let input = Rc::new(
+            TensorBuilder::new(array![[1e-10, 2e-9, 3e-3, 0., 0., 0., 0., 0.]].into_dyn()).build(),
+        );
+        head.forward(input);
+    }
+
+    #[test]
+    fn test_causal_masking() {
+        let head = AttentionHead::new("test_attention_head", 8, 4);
+        let input_1 = Rc::new(
+            TensorBuilder::new(
+                array![
+                    [1., 2., 3., 4., 5., 6., 7., 8.],
+                    [9., 8., 7., 6., 5., 4., 3., 2.]
+                ]
+                .into_dyn(),
+            )
+            .build(),
+        );
+        let input_2 = Rc::new(
+            TensorBuilder::new(
+                array![
+                    [1., 2., 3., 4., 5., 6., 7., 8.],
+                    // second token is different
+                    [5., 0., 5., 0., 5., 0., 5., 0.]
+                ]
+                .into_dyn(),
+            )
+            .build(),
+        );
+        let raw_output_1 = head.forward(input_1);
+        let raw_output_2 = head.forward(input_2);
+
+        let output_1 = raw_output_1.array.borrow();
+        let output_2 = raw_output_2.array.borrow();
+
+        // The causal mask prevents the first token's output from depending on
+        // subsequent tokens. (QK^T will be different, but all the particular
+        // entries that make it different get masked out.)
+
+        let first_token_output_1 = output_1.slice(s![0, ..]);
+        let first_token_output_2 = output_2.slice(s![0, ..]);
+        assert_eq!(first_token_output_1, first_token_output_2);
+
+        let second_token_output_1 = output_1.slice(s![1, ..]);
+        let second_token_output_2 = output_2.slice(s![1, ..]);
+        assert_ne!(second_token_output_1, second_token_output_2);
+    }
 }
